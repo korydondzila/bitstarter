76b773d82bd3299ddc54997fc1ca1e5104057760
Kory Dondzila <korydondzila@gmail.com>
2013-07-11 02:44:19 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync("index.html"));
+var buffer = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
