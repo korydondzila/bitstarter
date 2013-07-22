e72e5ad997fee1c8ca3d8056541c32e5fb0d1980
Kory Dondzila <korydondzila@gmail.com>
2013-07-22 05:02:25 +0000

    Started styling my bitstarter.

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 var buffer = new Buffer(fs.readFileSync('index.html'));
 
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
   response.send(text);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
